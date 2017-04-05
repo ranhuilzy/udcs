6de74259c98ad855d67e350d4ddd6455c4bd145f
ranhui <hui.ran@msxf.com>
2017-04-05 18:13:04 +0800

    [+]新增RabbitAmq配置

---
+++ udcs-web/src/main/java/com/rh/udcs/web/configs/AmqpConfig.java
@@ -0,0 +1,118 @@
+package com.rh.udcs.web.configs;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.messaging.handler.annotation.Payload;
+
+/**
+ * Created by hui.ran on 2017/4/5.
+ */
+@Configuration
+public class AmqpConfig {
+    public static final String FOO_EXCHANGE   = "callback.exchange.foo";
+    public static final String FOO_ROUTINGKEY = "callback.routingkey.foo";
+    public static final String FOO_QUEUE      = "callback.queue.foo";
+
+    @Value("${spring.rabbitmq.addresses}")
+    private String addresses;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
+
+    @Value("${spring.rabbitmq.publisher-confirms}")
+    private boolean publisherConfirms;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses(addresses);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost(virtualHost);
+        /** 如果要进行消息回调，则这里必须要设置为true */
+        connectionFactory.setPublisherConfirms(publisherConfirms);
+        return connectionFactory;
+    }
+
+    /** 设置交换机类型  */
+    @Bean
+    public DirectExchange defaultExchange() {
+        /**
+         * DirectExchange:按照routingkey分发到指定队列
+         * TopicExchange:多关键字匹配
+         * FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
+         * HeadersExchange ：通过添加属性key-value匹配
+         */
+        return new DirectExchange(AmqpConfig.FOO_EXCHANGE);
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer messageContainer() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
+        container.setQueues(fooQueue());
+        container.setExposeListenerChannel(true);
+        container.setMaxConcurrentConsumers(1);
+        container.setConcurrentConsumers(1);
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL); //设置确认模式手工确认
+        container.setMessageListener((ChannelAwareMessageListener) (message, channel) -> {
+            byte[] body = message.getBody();
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); //确认消息成功消费
+        });
+        return container;
+    }
+
+    @Bean
+    public Queue fooQueue() {
+        return new Queue(AmqpConfig.FOO_QUEUE);
+    }
+
+    @Bean
+    public Binding binding() {
+        /** 将队列绑定到交换机 */
+        return BindingBuilder.bind(fooQueue()).to(defaultExchange()).with(AmqpConfig.FOO_ROUTINGKEY);
+    }
+
+    @RabbitHandler
+    public void process(@Payload String foo) {
+//        LOGGER.info(“Listener: “ + foo);
+    }
+
+    @Bean
+    /** 因为要设置回调类，所以应是prototype类型，如果是singleton类型，则回调类为最后一次设置 */
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMaxConcurrentConsumers(20);
+        factory.setConcurrentConsumers(5);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setMessageConverter(messageConverter);
+        return factory;
+    }
+
+}
---
+++ udcs-web/src/main/java/com/rh/udcs/web/configs/AppAmqListener.java
@@ -0,0 +1,7 @@
+package com.rh.udcs.web.configs;
+
+/**
+ * Created by hui.ran on 2017/4/5.
+ */
+public class AppAmqListener {
+}
